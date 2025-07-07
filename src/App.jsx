e5176c46baa089a50f5a13c2dbfa773ad9d578a7
Anoop Vikram <anoopvikram619@gmail.com>
2025-07-07 15:21:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {ScrollTrigger, SplitText} from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+export const App = () => {
+  return (
+    <div>App</div>
+  )
+}
