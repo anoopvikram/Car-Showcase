ed714f8c6f4e326db92130fe8e087df5209ff156
Anoop Vikram <anoopvikram619@gmail.com>
2025-07-11 12:34:43 +0530

    animated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,29 @@
 import {navLinks} from "../../constants/index"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
 
 export const Navbar = () => {
+
+    useGSAP(()=>{
+        const navTween = gsap.timeline({
+            scrollTrigger:{
+                trigger: 'main',
+                start: "top+=1 top",
+                end: 'top top',
+                toggleActions: 'play none none reverse'
+            }
+        });
+
+        navTween.fromTo('nav',
+            {backgroundColor: 'transparent'},
+            {
+                backgroundColor: '#00000050', 
+                backdropFilter: 'blur(10px)',
+                border: '1px solid #333',
+                duration: .7,
+                ease: 'power1.inOut'
+            })
+    })
   return (
     <nav>
         <div>
